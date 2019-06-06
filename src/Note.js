bb0c349085103541acd45a3eb498b3e614e9d388
atotputerNICA <nicadanamaria@gmail.com>
2019-06-06 03:06:08 +0300

    added delete and add note

--- src/Note.js
+++ src/Note.js
@@ -3,16 +3,48 @@ import { LitElement, html, css } from 'lit-element';
 class Note extends LitElement {
   static get styles() {
     return css`
-      :host { /* for space-data*/
-        background: #bdcebe;
-        padding: 1rem;
-        margin: 0.5rem;
-        height: max-content;
 
+      :host {
+        margin: 1rem;
       }
-      :host(:hover) {
-        box-shadow: 10px 10px 5px #aaaaaa;
+
+      div{
+        display:inline-block;
+        position:relative;
       }
+
+      span {
+        position:absolute;
+        top:-10px;
+        right:10px;
+        font:48px 'Gloria Hallelujah', cursive;
+        color: rgb(185, 55, 55);
+      }
+      span:hover {
+        color: rgb(122, 155, 252);
+      }
+
+      textarea {
+        display:block;
+        padding:40px 40px 40px;
+        width:200px;
+        height:200px;
+        font:20px 'Gloria Hallelujah', cursive;
+        line-height:1.5;
+        border:0;
+        border-radius:3px;
+        background: linear-gradient(#F9EFAF, #F7E98D);
+        box-shadow:0 10px 10px rgba(0,0,0,0.1);
+        overflow:hidden;
+        transition:box-shadow 0.5s ease;
+        font-smoothing:subpixel-antialiased;
+        /* max-width:520px;
+        max-height:250px; */
+      }
+      textarea:hover { box-shadow:0 5px 8px rgba(0,0,0,0.15); }
+      textarea:focus { box-shadow:0 5px 12px rgba(0,0,0,0.2); outline:none; }
+
+
     `;
   }
 
@@ -24,7 +56,10 @@ class Note extends LitElement {
 
   render() {
     return html`
-      <div><output>${this.text}</output></div>
+      <div>
+        <textarea contenteditable="true">${this.text}</textarea>
+        <span class="close">&times;</span>
+      </div>
     `;
   }
 }
--- src/NoteMain.js
+++ src/NoteMain.js
@@ -10,14 +10,19 @@ export class NoteMain extends LitElement {
         /* background: lightblue; */
         display: flex;
         flex-direction: row;
+        flex-wrap: wrap;
         padding: 1rem;
         background: #cce4ce;
+        /* background:url(https://subtlepatterns.com/patterns/little_pluses.png) #cacaca; */
+
 
       }
 
       @media only screen and (max-width: 600px)  {
         :host {
           flex-direction: column;
+          /* flex-wrap: wrap; */
+
         }
       }
     `;
